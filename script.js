e9b3c9e5076c53239dbfd2c31c92593fd5e6e2e0
Hisham <hsim2446@gmail.com>
2025-08-21 21:35:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("download-pdf").addEventListener("click", () => {
+    const { jsPDF } = window.jspdf;
+    const cv = document.querySelector(".cv-container"); // حصر المحتوى فقط
+
+    html2canvas(cv, { scale: 3 }).then(canvas => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("p", "mm", "a4");
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = pdf.internal.pageSize.getHeight();
+        const imgWidth = pdfWidth;
+        const imgHeight = (canvas.height * pdfWidth) / canvas.width;
+
+        let heightLeft = imgHeight;
+        let position = 0;
+
+        pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+        heightLeft -= pdfHeight;
+
+        while (heightLeft > 0) {
+            position = heightLeft - imgHeight;
+            pdf.addPage();
+            pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
+            heightLeft -= pdfHeight;
+        }
+
+        pdf.save("Hisham_Full_CV.pdf");
+    });
+});
